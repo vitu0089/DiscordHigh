300712df9c68c8c973397c01495e6b6d5cef0e8d
vitu0089 <gamerw30@gmail.com>
2022-04-29 14:59:32 +0200

    LUOTD 290422

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.VS = exports.HLD = void 0;
 const node_json_db_1 = require("node-json-db");
 class HLD {
     constructor(client) {
@@ -17,7 +18,38 @@ class HLD {
         var user_path = "/" + user_id;
         return this.DB.Members.exists(user_path) && this.DB.Members.getData(user_path) || if_null || false;
     }
-    setUser(user_id, if_null) {
+    setUser(user_id, data) {
+        var user_path = "/" + user_id;
+        this.DB.Members.push(user_path, data, true);
+        return data;
     }
 }
-exports.default = HLD;
+exports.HLD = HLD;
+class VS {
+    constructor(main_module, points_name, start_value, options) {
+        this.name = points_name;
+        this.default_value = start_value;
+        this.options = options;
+        this.main_module = main_module;
+    }
+    increment(user_id, value) {
+        if (!this.options || this.options && !this.options.incremental) {
+            throw "Trying to increment a non-incrementable value";
+        }
+        const player_data = this.main_module.getUser(user_id);
+        if (!player_data) {
+            throw "Couldn't find player_data";
+        }
+        if (!player_data[this.name]) {
+            player_data[this.name] = this.default_value;
+        }
+        const possible_transaction = player_data[this.name] + value >= 0;
+        if (possible_transaction)
+            player_data[this.name] += value;
+        return possible_transaction;
+    }
+    getValue(user_id) {
+        return this.main_module.getUser(user_id)[this.name] || this.increment(user_id, 0) && this.default_value;
+    }
+}
+exports.VS = VS;
--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import Discord from "discord.js"
 import { JsonDB as OverallDB } from "node-json-db"
 
-export default class HLD {
+interface IncrementOptionsInterface{
+    incremental?:boolean
+}
+
+export class HLD {
     client:Discord.Client
     DB:{Members:OverallDB,Servers:OverallDB,Global:OverallDB}
     constructor(client:Discord.Client){
@@ -21,7 +25,47 @@ export default class HLD {
         var user_path = "/" + user_id
         return this.DB.Members.exists(user_path) && this.DB.Members.getData(user_path) || if_null || false
     }
-    setUser(user_id:string,if_null?:any){
+    setUser(user_id:string,data:any){
+        var user_path = "/" + user_id
+        this.DB.Members.push(user_path,data,true)
+        return data
+    }
+}
 
+export class VS {
+    name:string
+    default_value:number
+    options:IncrementOptionsInterface | undefined
+    main_module:HLD
+
+    constructor(main_module:HLD,points_name:string,start_value:number,options?:IncrementOptionsInterface | undefined){
+        this.name = points_name
+        this.default_value = start_value
+        this.options = options
+        this.main_module = main_module
+    }
+
+    increment(user_id:string,value:number){
+        if (!this.options || this.options && !this.options.incremental){
+            throw "Trying to increment a non-incrementable value"
+        }
+        
+        const player_data = this.main_module.getUser(user_id)
+
+        if (!player_data) {
+            throw "Couldn't find player_data"
+        }
+        
+        if (!player_data[this.name]){
+            player_data[this.name] = this.default_value
+        }
+
+        const possible_transaction = player_data[this.name] + value >= 0
+        if (possible_transaction) player_data[this.name] += value
+        return possible_transaction
+    }
+
+    getValue(user_id:string){
+        return this.main_module.getUser(user_id)[this.name] || this.increment(user_id,0) && this.default_value
     }
 }
